fa5ef3dde926eebd7ba75a2df3dcb623f8b1da1a
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-08-29 11:39:05 -0300

    Dreamcast: add PVR2 scan timing state

--- src/core/dreamcast_pvr2.h
+++ src/core/dreamcast_pvr2.h
@@ -7,6 +7,13 @@
 
 namespace jojo {
 
+struct DreamcastPvr2ScanTiming {
+    std::uint32_t cycles_per_scanline{};
+    std::uint32_t scanlines_per_frame{};
+    std::uint32_t vblank_begin_scanline{};
+    std::uint32_t vblank_end_scanline{};
+};
+
 class DreamcastPvr2 final : public DreamcastMmioDevice {
 public:
     explicit DreamcastPvr2(DreamcastSystemAsic& system_asic) noexcept
@@ -16,11 +23,17 @@ public:
     [[nodiscard]] Result<void> write8(std::uint32_t address, std::uint8_t value) override;
 
     void vblank_begin() noexcept;
+    void configure_scan_timing(DreamcastPvr2ScanTiming timing) noexcept;
+    void advance_cycles(std::uint32_t cycles) noexcept;
 
     static constexpr std::uint32_t kNormalEventVblankBegin = 1u << 3u;
+    static constexpr std::uint32_t kNormalEventVblankEnd = 1u << 4u;
 
 private:
     DreamcastSystemAsic* system_asic_{};
+    DreamcastPvr2ScanTiming scan_timing_{};
+    std::uint32_t scanline_{};
+    std::uint32_t cycles_into_scanline_{};
 };
 
 }
