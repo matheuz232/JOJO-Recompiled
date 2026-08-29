78fce62d255baf347fe3018c4a72e69182bab338
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-08-29 05:03:12 -0300

    feat: declare Dreamcast System ASIC interrupt device

---
+++ src/core/dreamcast_system_asic.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "core/dreamcast_bus.h"
+
+#include <array>
+#include <cstdint>
+
+namespace jojo {
+
+class DreamcastSystemAsic final : public DreamcastMmioDevice {
+public:
+    [[nodiscard]] Result<std::uint8_t> read8(std::uint32_t address) override;
+    [[nodiscard]] Result<void> write8(std::uint32_t address, std::uint8_t value) override;
+
+    void raise_normal(std::uint32_t bits) noexcept { status_[0] |= bits; }
+    void raise_external(std::uint32_t bits) noexcept { status_[1] |= bits; }
+    void raise_error(std::uint32_t bits) noexcept { status_[2] |= bits; }
+
+private:
+    [[nodiscard]] static std::uint32_t physical_address(std::uint32_t address) noexcept;
+    [[nodiscard]] static int status_index(std::uint32_t address) noexcept;
+    [[nodiscard]] static int mask_index(std::uint32_t address) noexcept;
+
+    std::array<std::uint32_t, 3> status_{};
+    std::array<std::uint32_t, 9> masks_{};
+};
+
+}
