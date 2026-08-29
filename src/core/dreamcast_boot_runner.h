5ab010eddfcf8a6ddcc590951c2fad85cdb4d8b7
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-08-29 02:55:22 -0300

    impl: define Dreamcast boot harness API

---
+++ src/core/dreamcast_boot_runner.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "core/dreamcast_memory.h"
+#include "core/result.h"
+#include "core/sh4_reference_executor.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+
+namespace jojo {
+
+enum class DreamcastBootStopReason {
+    end_of_program,
+    left_program,
+    block_limit,
+    unsupported_opcode,
+};
+
+struct DreamcastBootRunResult {
+    DreamcastExecutableMemory memory;
+    Sh4ReferenceState state;
+    DreamcastBootStopReason stop_reason{DreamcastBootStopReason::end_of_program};
+    std::size_t blocks_executed{};
+    std::size_t operations_executed{};
+    std::optional<std::uint32_t> unsupported_address;
+    std::optional<std::uint16_t> unsupported_raw;
+};
+
+[[nodiscard]] Result<DreamcastBootRunResult> run_dreamcast_boot_reference(
+    const DreamcastBootProgram& program,
+    Sh4ReferenceState initial_state = {},
+    std::size_t max_blocks = 100000u);
+
+}
