efab64a08e8dffddb57d32069af4c5e4a5ab5cd3
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-08-30 16:04:27 -0300

    feat: inline supported PS1 US revision profile

--- src/core/psx_revision.h
+++ src/core/psx_revision.h
@@ -4,6 +4,17 @@
 
 namespace jojo {
 
-[[nodiscard]] const std::vector<GameRevisionProfile>& supported_psx_game_revision_profiles();
+[[nodiscard]] inline const std::vector<GameRevisionProfile>& supported_psx_game_revision_profiles() {
+    static const std::vector<GameRevisionProfile> profiles = {
+        {
+            "ps1-usa-slus-01060",
+            {
+                {"/SYSTEM.CNF", 68u, 0x1eb36f6335bbf54aull},
+                {"/SLUS_010.60", 565248u, 0xb84be235e572adccull},
+            },
+        },
+    };
+    return profiles;
+}
 
 }
