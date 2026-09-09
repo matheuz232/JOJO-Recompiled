ac5782e30baed854624da27bb5c9f93c8c1ce2f5
matheuz232 <82462256+matheuz232@users.noreply.github.com>
2026-09-09 04:15:34 -0300

    feat: declare R3000A PS-X EXE initializer

--- src/core/r3000a_reference_executor.h
+++ src/core/r3000a_reference_executor.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "core/ps1_exe.h"
 #include "core/r3000a_bus.h"
 #include "core/r3000a_diagnostics.h"
 #include "core/r3000a_state.h"
@@ -7,5 +8,6 @@
 namespace jojo {
 
 [[nodiscard]] R3000aStepResult step_r3000a(R3000aState& state, R3000aBus& bus) noexcept;
+[[nodiscard]] R3000aState initialize_r3000a_for_psx_exe(const Ps1ExeMetadata& metadata) noexcept;
 
 } // namespace jojo
